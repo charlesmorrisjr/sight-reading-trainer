17fc9e88bdfe5a0406f0b7c1b3345d79a6e1c8cb
Charles Morris Jr <cmorrisjr777@gmail.com>
2025-12-25 22:06:48 -0500

    Created SheetMusic component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-import { useEffect, useState, useRef } from 'react'
+import { useState } from 'react'
 import './App.css'
 
-import abcjs from "abcjs";
-
 import generateAbc from './musicGenerator';
+import SheetMusic from './components/SheetMusic';
 
 function App() {
   const [abcNotation, setAbcNotation] = useState(generateAbc());
-  const sheetMusicRenderRef = useRef(null);
-    
-  // Update the rendered sheet music every time the ABC notation changes
-  useEffect(() => {
-    // Guard clause in case sheetMusicRenderRef is not defined
-    if (!sheetMusicRenderRef.current) return;
-
-    abcjs.renderAbc(sheetMusicRenderRef.current, abcNotation);
-    
-    console.log(abcNotation); // Debug
-  }, [abcNotation]);
 
   return (
     <>
       <h1>Sight Reading Trainer</h1>
-      <div ref={sheetMusicRenderRef}></div>
+      <SheetMusic abcNotation={abcNotation}/>
       <button className="button" onClick={() => setAbcNotation(generateAbc())}>Generate New Exercise</button>
     </>
   )
---
+++ src/components/SheetMusic.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react'
+import './App.css'
+
+import abcjs from "abcjs";
+
+// Render sheet music
+export default function SheetMusic({ abcNotation }) {
+  const sheetMusicRenderRef = useRef(null);
+    
+  // Update the rendered sheet music every time the ABC notation changes
+  useEffect(() => {
+    // Guard clause in case sheetMusicRenderRef is not defined
+    if (!sheetMusicRenderRef.current) return;
+
+    abcjs.renderAbc(sheetMusicRenderRef.current, abcNotation);
+    
+    console.log(abcNotation); // Debug
+  }, [abcNotation]);
+
+  return (
+    <>
+      <div ref={sheetMusicRenderRef}></div>
+    </>
+  )
+}
